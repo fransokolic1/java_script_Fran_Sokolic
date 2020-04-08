53d59eb44a057d9e6b47c1c928d7f7f4de59a59a
fransokolic1 <55946414+fransokolic1@users.noreply.github.com>
2020-04-08 21:45:17 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+alert("PORUKA KOJA SE PRIKAZUJE U PROZORU");
+confirm("ODABERITE:");
+prompt("UNESITE VAŠU DOB","10");
+document.write("TEST ISPISIVANJA");
+function funkcija(){
+    document.getElementById("test").innerHTML = "ODLOMAK SE PROMJENIO";
+}
+console.log("OVA PORUKA JE VIDLJIVA");
